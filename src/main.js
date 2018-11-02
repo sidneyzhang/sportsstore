085dda49b00a96f033e0742d4832297df55ebbc7
sidney zhang <sidneyzsc@hotmail.com>
2018-11-02 18:17:19 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css"
+
+import store from "./store";
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    store
 }).$mount('#app')
